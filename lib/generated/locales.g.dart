3af8f36d9732165dded445e4ca69944584cc763d
DanhDue ExOICTIF <danhdue@gmail.com>
2022-10-05 00:12:48 +0700

    add the locales for the translation.

---
+++ lib/generated/locales.g.dart
@@ -0,0 +1,24 @@
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+// ignore_for_file: lines_longer_than_80_chars
+// ignore: avoid_classes_with_only_static_members
+class AppTranslation {
+  static Map<String, Map<String, String>> translations = {
+    'vn_VI': Locales.vn_VI,
+    'en_US': Locales.en_US,
+  };
+}
+
+class LocaleKeys {
+  LocaleKeys._();
+  static const homePageTitle = 'homePageTitle';
+}
+
+class Locales {
+  static const vn_VI = {
+    'homePageTitle': 'Đăng nhập - Trang chủ',
+  };
+  static const en_US = {
+    'homePageTitle': 'Login - Home Page',
+  };
+}
