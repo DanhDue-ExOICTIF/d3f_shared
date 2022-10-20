7c532648899d89cf242594f738251bac14c03ab3
DanhDue ExOICTIF <danhdue@gmail.com>
2022-10-20 09:53:21 +0700

    configure the shared assets via the flutter_gen.

--- lib/extensions/string_ext.dart
+++ lib/extensions/string_ext.dart
@@ -1,5 +1,5 @@
-extension StringExtension on String {
-  String get imageOriginal {
+extension StringExtension on String? {
+  String? get imageOriginal {
     return 'https://image.tmdb.org/t/p/original$this';
   }
 }
