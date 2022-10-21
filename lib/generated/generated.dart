4686c74c413edc84625af03b43ec153cf7b0487a
DanhDue ExOICTIF <danhdue@gmail.com>
2022-10-21 11:45:15 +0700

    add fonts to the project.

--- lib/generated/generated.dart
+++ lib/generated/generated.dart
@@ -1,2 +1,3 @@
-export 'package:d3f_shared/generated/assets.gen.dart';
 export 'package:d3f_shared/generated/locales.g.dart';
+export 'package:d3f_shared/generated/assets.gen.dart';
+export 'package:d3f_shared/generated/fonts.gen.dart';
